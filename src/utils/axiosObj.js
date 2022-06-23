ef44d7e9a1c019dda11b1c1f8f2fb7342fa6f846
KyiThantSin <kyithantsin@mtu.edu.mm>
2022-06-23 22:04:24 +0630

    change API and Scripts

--- src/utils/axiosObj.js
+++ src/utils/axiosObj.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosObj = axios.create({
-    baseURL:`https://food-order-v0.herokuapp.com/menu`,
+    baseURL:`https://food-order-v0.herokuapp.com/menu/api`,
     headers:{
         'Content-type': 'application/json',
     }
